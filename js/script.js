de0d6816d45d87be13c87ed50db4a5aaa80c8426
k1slim <k1slim@yandex.ru>
2014-07-08 18:03:48 +0400

    2

--- js/script.js
+++ js/script.js
@@ -8,11 +8,22 @@
         elem1.style.display='none';
         elem2.style.display='block';
         numb++;
-        alert(numb);
+        app.addInfo('numb',numb);
     };
+
+    app.addInfo=function(id,per){
+        document.getElementById(id).innerHTML=per;
+    };
+    app.rightAnsv=function(e){
+        var qwerty=document.getElementById('ansv');
+
+    }
+
 
     root.code=app;
 
 }(window));
 
+            /*==Listeners==*/
+document.getElementById('test1').addEventListener( "click", code.openTest, false );
 document.getElementById('test1').addEventListener( "click", code.openTest, false );
