7eedd3089797698bca8d8ab0b307768a45fd6162
k1slim <k1slim@yandex.ru>
2014-07-09 13:02:20 +0400

    Добавлены миксины в отдельном файле. Добавлены переменные в отдельном .less-файле. Создан файл data.js, содержащий исходную информацию. Добавлен .gitignore. Переделано добавление событий на кнопки главного экрана. Добавлена функция,которая распознаёт выбранную тему и пишет её название в заголовке экрана с вопросами.

---
+++ js/data.js
@@ -0,0 +1,182 @@
+var quizData = [
+    {
+        title     : "Проезд перекрестков",
+        questions : [
+            {
+                content : "Текст вопроса1",
+                img     : "path/img.png",
+                right   : 1,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                    ]
+            },
+            {
+                content : "Текст вопроса2",
+                img     : "path/img.png",
+                right   : 3,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса3",
+                img     : "path/img.png",
+                right   : 2,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            }
+
+        ]
+    },
+    {
+        title     : "Медицины",
+        questions : [
+            {
+                content : "Текст вопроса1",
+                img     : "path/img.png",
+                right   : 1,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса2",
+                img     : "path/img.png",
+                right   : 3,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса3",
+                img     : "path/img.png",
+                right   : 2,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            }
+
+        ]
+    },
+    {
+        title     : "Осветительные приборы",
+        questions : [
+            {
+                content : "Текст вопроса1",
+                img     : "path/img.png",
+                right   : 1,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса2",
+                img     : "path/img.png",
+                right   : 3,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса3",
+                img     : "path/img.png",
+                right   : 2,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            }
+
+        ]
+    },
+    {
+        title     : "бла-бла",
+        questions : [
+            {
+                content : "Текст вопроса1",
+                img     : "path/img.png",
+                right   : 1,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса2",
+                img     : "path/img.png",
+                right   : 3,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса3",
+                img     : "path/img.png",
+                right   : 2,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            }
+
+        ]
+    },
+    {
+        title     : "бла-бла-бла-бла",
+        questions : [
+            {
+                content : "Текст вопроса1",
+                img     : "path/img.png",
+                right   : 1,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса2",
+                img     : "path/img.png",
+                right   : 3,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            },
+            {
+                content : "Текст вопроса3",
+                img     : "path/img.png",
+                right   : 2,
+                answers : [
+                    "вариант 1",
+                    "вариант 2",
+                    "вар 3"
+                ]
+            }
+
+        ]
+    }
+];
--- js/script.js
+++ js/script.js
@@ -2,28 +2,55 @@
 
     var app={},numb=0;
 
-    app.openTest=function(){
+    app.openTest=function(nTest){
         var elem1=document.getElementById('leftBlock');
         var elem2=document.getElementById('Question');
         elem1.style.display='none';
         elem2.style.display='block';
         numb++;
         app.addInfo('numb',numb);
+        app.placeData('titlePlaceholder',quizData[nTest-1].title);
     };
 
     app.addInfo=function(id,per){
         document.getElementById(id).innerHTML=per;
     };
-    app.rightAnsv=function(e){
+
+    app.placeData=function(plhold,data){
+        document.getElementById(plhold).innerHTML=data;
+    };
+
+
+   /* app.rightAnsv=function(e){
         var qwerty=document.getElementById('ansv');
 
-    }
+    };
 
+    app.addInfo=function(text,flag,rAnsv){
+
+    };*/
 
     root.code=app;
 
 }(window));
 
+
+window.onload=function(){
+    var elems=document.getElementsByClassName('testStr');
+    for(var i=0;i<elems.length;i++){
+        document.getElementById(elems[i].id).addEventListener("click",function(){code.openTest(parseInt(this.id))});
+
+
+
+    }
+
+
+
+}
             /*==Listeners==*/
-document.getElementById('test1').addEventListener( "click", code.openTest, false );
-document.getElementById('test1').addEventListener( "click", code.openTest, false );
+/*document.getElementById('1test').addEventListener( "click",function(){code.openTest(1)}, false );
+document.getElementById('2test').addEventListener( "click", function(){code.openTest(2)}, false );
+document.getElementById('3test').addEventListener( "click", function(){code.openTest(3)}, false );
+document.getElementById('4test').addEventListener( "click", function(){code.openTest(4)}, false );
+document.getElementById('5test').addEventListener( "click", function(){code.openTest(5)}, false );*/
+
