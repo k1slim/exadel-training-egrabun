d36c41cf7939eec8b33926cc67bac88e636b56ce
k1slim <k1slim@yandex.ru>
2014-07-18 15:35:16 +0400

    Переделана логика работы приложения Исправления и дополнения для работы persistence модуля

--- js/test.js
+++ js/test.js
@@ -9,6 +9,7 @@
         this.statModule = new StatModule();
 
         this.activeQuestion=0;
+        this.answArr=[];
     };
 
     TestModule.prototype.placeQuestions=function(elem){
@@ -29,79 +30,108 @@
             util.toggle(i+'answ','open');
             util.placeData(i+'answ',elem.answers[i]);
         }
+        quiz.persModule.get(this.statModule.getStats(),this.activeQuestion,quiz.numberOfTest,this.answArr,this.statModule.quizzes);
+        this.statModule.updateStats();
     };
 
     TestModule.prototype.determineAnswNumber=function(e){
         if(e.target.classList.contains('answ') || e.target.id == 'skip')
         {
             this.determinateRightAnswer(e);
-            this.changeFlag();
-            this.searchSkippedQuest();
-            console.log('activeQuestion=',this.activeQuestion,'nTestGlobal=',quiz.numberOfTest,'noAnswered=',quiz.data[quiz.numberOfTest].questions[this.activeQuestion].noAnswered,'flag=',quiz.data[quiz.numberOfTest].EndOfTestFlag);
+            console.log('activeQuestion=',this.activeQuestion,'nTestGlobal=',quiz.numberOfTest,'Answered=',quiz.data[quiz.numberOfTest].questions[this.activeQuestion].Answered);
         }
     };
 
     TestModule.prototype.determinateRightAnswer=function(e){
         if(e.target.id != 'skip')
         {
-            if(parseInt(e.target.id)+1==quiz.data[quiz.numberOfTest].questions[this.activeQuestion].right)
-                this.statModule.increaseParameter(2);
+            this.statModule.increaseParameter(StatModule.statItems.NUMBER);
+            quiz.data[quiz.numberOfTest].questions[this.activeQuestion].Answered=1;
+            this.answArr.push(this.activeQuestion);
+
+            if(parseInt(e.target.id)+1==quiz.data[quiz.numberOfTest].questions[this.activeQuestion].right){
+                this.statModule.increaseParameter(StatModule.statItems.RIGHT);
+                this.logicOfQuestions();
+            }
             else{
-                this.statModule.increaseParameter(3);
+                this.statModule.increaseParameter(StatModule.statItems.WRONG);
                 util.showAlertWindow('open',quiz.data[quiz.numberOfTest].questions[this.activeQuestion].question,1,quiz.data[quiz.numberOfTest].questions[this.activeQuestion].answers[quiz.data[quiz.numberOfTest].questions[this.activeQuestion].right-1],quiz.data[quiz.numberOfTest].questions[this.activeQuestion].answers[parseInt(e.target.id)]);
             }
-
-            this.statModule.increaseParameter(1);
-        }
-        else
-            quiz.data[quiz.numberOfTest].questions[this.activeQuestion].noAnswered=1;
-    };
-
-    TestModule.prototype.changeFlag=function(){
-        if(this.activeQuestion==quiz.data[quiz.numberOfTest].questions.length-1 && quiz.data[quiz.numberOfTest].EndOfTestFlag!=1 && this.statModule.numberOfAnswQuest < quiz.data[quiz.numberOfTest].questions.length+1)
-        {
-            quiz.data[quiz.numberOfTest].EndOfTestFlag=1;
-            util.showAlertWindow('open','Пропущенные вопросы',0);
         }
         else{
-            if(this.statModule.numberOfAnswQuest!=quiz.data[quiz.numberOfTest].questions.length+1 && quiz.data[quiz.numberOfTest].EndOfTestFlag!=1){
-                this.activeQuestion++;
-                this.placeQuestions(quiz.data[quiz.numberOfTest].questions[this.activeQuestion]);
-            }
-        }
-
-        if(this.statModule.numberOfAnswQuest==quiz.data[quiz.numberOfTest].questions.length+1)
-        {
-            util.showAlertWindow('open','Молодэц,правильных ответов - '+this.statModule.rightAnsw+'.',0);
-            this.returnToMainPage();
+            this.logicOfQuestions();
         }
     };
 
-    TestModule.prototype.searchSkippedQuest=function(){
-        if(quiz.data[quiz.numberOfTest].EndOfTestFlag==1){
-            for(var i=0;i<quiz.data[quiz.numberOfTest].questions.length;i++){
-                if(quiz.data[quiz.numberOfTest].questions[i].noAnswered==1){
-                    quiz.data[quiz.numberOfTest].questions[i].noAnswered=0;
+    TestModule.prototype.logicOfQuestions=function(){
+        var found = false;
+
+        if(this.statModule.numberOfAnswQuest != quiz.data[quiz.numberOfTest].questions.length+1){
+
+            if(this.activeQuestion==quiz.data[quiz.numberOfTest].questions.length-1){
+                for(var j=0;j<quiz.data[quiz.numberOfTest].questions.length;j++){
+                    if(!quiz.data[quiz.numberOfTest].questions[j].Answered){
+                        this.activeQuestion=j;
+                        this.placeQuestions(quiz.data[quiz.numberOfTest].questions[this.activeQuestion]);
+                        break;
+                    }
+                }
+
+            }
+            else{
+                this.activeQuestion++;
+
+                for(var i=this.activeQuestion;i<quiz.data[quiz.numberOfTest].questions.length;i++){
+                    if(!quiz.data[quiz.numberOfTest].questions[i].Answered){
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(found === false){
+                    this.logicOfQuestions();
+                }
+                else{
                     this.activeQuestion=i;
-                    break;
+                    this.placeQuestions(quiz.data[quiz.numberOfTest].questions[this.activeQuestion]);
                 }
             }
-            this.placeQuestions(quiz.data[quiz.numberOfTest].questions[this.activeQuestion]);
         }
+         if(this.statModule.numberOfAnswQuest == quiz.data[quiz.numberOfTest].questions.length+1){
+         util.showAlertWindow('open','Молодэц,правильных ответов - '+this.statModule.rightAnsw+'.',0);
+         this.returnToMainPage();
+         }
     };
 
     TestModule.prototype.returnToMainPage=function(){
+        if(this.statModule.numberOfAnswQuest == quiz.data[quiz.numberOfTest].questions.length+1)
+            this.statModule.markPassedTest(quiz.numberOfTest);
+
         util.toggle('leftBlock','open');
         util.toggle('question','close');
         util.toggle('back','close');
+        util.toggle('info','close');
 
         this.activeQuestion=0;
         this.statModule.resetStats();
         this.statModule.updateStats();
 
+        this.answArr=[];
         for(var i=0;i<quiz.data[quiz.numberOfTest].questions.length;i++)
-           delete quiz.data[quiz.numberOfTest].questions[i].noAnswered;
-        delete quiz.data[quiz.numberOfTest].EndOfTestFlag;
+           delete quiz.data[quiz.numberOfTest].questions[i].Answered;
+        quiz.persModule.clearStorage();
+    };
+
+    TestModule.prototype.defineClosedButtonAction=function(){
+        if(this.statModule.numberOfAnswQuest == quiz.data[quiz.numberOfTest].questions.length+1){
+            util.showAlertWindow('open','Молодэц,правильных ответов - '+this.statModule.rightAnsw+'.',0);
+            this.returnToMainPage();
+        }
+        else{
+            if(this.statModule.numberOfAnswQuest != 0)
+                this.logicOfQuestions();
+            util.showAlertWindow('close');
+        }
     };
 
 
