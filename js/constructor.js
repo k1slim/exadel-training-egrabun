9d719841df3ba65b831f9c80ddd31bd46b63eb19
k1slim <k1slim@yandex.ru>
2014-07-18 15:34:26 +0400

    Добавлен метод для работы с persistence модулем Мелкие исправления и дополнения

--- js/constructor.js
+++ js/constructor.js
@@ -3,7 +3,8 @@
     var QuizApp=function(){
         this.contenerWithTests=document.getElementsByClassName('listTest')[0];
 
-        this.testModule = new TestModule();
+        this.testModule=new TestModule();
+        this.persModule=null;
 
         this.numberOfTest=-1;
         this.data={};
@@ -13,9 +14,13 @@
         util.toggle('leftBlock','close');
         util.toggle('question','open');
         util.toggle('back','open');
+        util.toggle('info','open');
+
         util.placeData('titlePlaceholder',this.data[this.numberOfTest].title);
-        this.testModule.placeQuestions(this.data[this.numberOfTest].questions[0]);
-        this.testModule.statModule.increaseParameter(1);
+        if(!this.persModule.checkLocalStorage())
+            this.testModule.statModule.increaseParameter(StatModule.statItems.NUMBER);
+        this.testModule.placeQuestions(this.data[this.numberOfTest].questions[this.testModule.activeQuestion]);
+
     };
 
     QuizApp.prototype.determineTestNumber=function(e){
@@ -26,46 +31,52 @@
     };
 
     QuizApp.prototype.init=function(){
+        var self=this;
+        this.persModule=new PersModule();
+
         this.getData();
-        util.placeInToContainer(this.contenerWithTests,this.data.length,"testStr",'test',0);
-        util.placeInToContainer(this.testModule.contenerWithQuestion,5,"answ open",'answ',2);
+        util.placeInToContainer(this.contenerWithTests,this.data.length,"testStr",'test',0,1);
+        util.placeInToContainer(this.testModule.contenerWithQuestion,5,"answ open",'answ',2,0);
 
         for(var i=0;i<this.data.length;i++){
             util.placeData(i+'test',this.data[i].title);
         }
 
-        this.contenerWithTests.addEventListener("click",function(evt){quiz.determineTestNumber(evt)});
-        this.testModule.contenerWithQuestion.addEventListener("click",function(evt){quiz.testModule.determineAnswNumber(evt)});
-        this.testModule.popUpCloseButton.addEventListener("click",function(){util.showAlertWindow('close')});
-        this.testModule.backButton.addEventListener("click",function(){quiz.testModule.returnToMainPage()});
+        this.contenerWithTests.addEventListener("click",function(evt){self.determineTestNumber(evt)});
+        this.testModule.contenerWithQuestion.addEventListener("click",function(evt){self.testModule.determineAnswNumber(evt)});
+        this.testModule.popUpCloseButton.addEventListener("click",function(){self.testModule.defineClosedButtonAction()});
+        this.testModule.backButton.addEventListener("click",function(){self.testModule.returnToMainPage()});
+        this.pushPersDataToApp();
+
     };
 
     QuizApp.prototype.getData=function ()
     {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "js/json/data.json",false);
-        xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-        xhr.onreadystatechange = function()
-        {
-            if(xhr.readyState === 4 && xhr.status === 200)
-            {
-                var type = xhr.getResponseHeader("Content-Type");
-                if(type.indexOf("xml") !== -1)
-                {
-                    quiz.data=xhr.responseXML;
-                }
-                else if(type === "application/json")
-                {
-                    quiz.data=JSON.parse(xhr.responseText);
-                }
-                else
-                {
-                    quiz.data=xhr.responseText;
-                }
-            }
-        };
-        xhr.send();
+        var self = this;
+
+        util.makeGETRequest("js/json/data.json", function(responseData){
+            self.data = responseData;
+        });
     };
+
+    QuizApp.prototype.pushPersDataToApp=function(){
+        if(this.persModule.checkLocalStorage()){
+            this.persModule.getFromLocalStorage();
+
+            this.numberOfTest=this.persModule.actTest;
+            this.testModule.activeQuestion=this.persModule.actQuest;
+            this.testModule.statModule.numberOfAnswQuest=this.persModule.stat.number;
+            this.testModule.statModule.rightAnsw=this.persModule.stat.right;
+            this.testModule.statModule.wrongAnsw=this.persModule.stat.wrong;
+            this.testModule.answArr=this.persModule.answArray;
+
+            this.openTest();
+
+            for(var i=0;i<this.testModule.answArr.length;i++)
+                this.data[this.numberOfTest].questions[this.testModule.answArr[i]].Answered=1;
+        }
+    };
+
 
     window.QuizzApp = QuizApp;
 
