7045d1b5cb3159beb76d41a4405734efddaf48e0
k1slim <k1slim@yandex.ru>
2014-08-06 13:07:43 +0400

    Добавлены отметки пройденных тестов

--- js/persistence.js
+++ js/persistence.js
@@ -1,44 +1,42 @@
-(function(win){
+define(
+    function(){
 
-    var PersModule=function(){
-        this.stat={};
-        this.actQuest=-1;
-        this.actTest=-1;
-        this.answArray=[];
+        var PersModule = function(){
+            this.stat = {};
+            this.actQuest = -1;
+            this.actTest = -1;
+            this.answArray = [];
+            this.passedTest = [];
 
-        this.parseLocalStorage();
-    };
+            this.parseLocalStorage();
+        };
 
-    PersModule.prototype.getToPersModule=function(statObj,actQuest,actTest,answArray){
-        this.stat=statObj;
-        this.actQuest=actQuest;
-        this.actTest=actTest;
-        this.answArray=answArray;
-    };
+        PersModule.prototype.getToPersModule = function(statObj, actQuest, actTest, answArray, passedTest){
+            this.stat = statObj;
+            this.actQuest = actQuest;
+            this.actTest = actTest;
+            this.answArray = answArray;
+            this.passedTest = passedTest;
+        };
 
-    PersModule.prototype.pushToLocalStorage=function(){
-        localStorage.setItem('quizzer',JSON.stringify(this));
-    };
+        PersModule.prototype.pushToLocalStorage = function(){
+            localStorage.setItem('quizzer', JSON.stringify(this));
+        };
 
-    PersModule.prototype.checkLocalStorage=function(){
-        return localStorage['quizzer'];
-    };
+        PersModule.prototype.checkLocalStorage = function(){
+            return localStorage['quizzer'];
+        };
 
-    PersModule.prototype.clearLocalStorage=function(){
-        localStorage.removeItem('quizzer');
-        this.getToPersModule({},-1,-1,[]);
-    };
-
-    PersModule.prototype.parseLocalStorage=function(){
-        if(this.checkLocalStorage()){
-            var temp=JSON.parse(localStorage.getItem('quizzer'));
-            this.getToPersModule(temp.stat,temp.actQuest,temp.actTest,temp.answArray);
-        }
-        else
-            this.getToPersModule({},-1,-1,[]);
-    };
+        PersModule.prototype.parseLocalStorage = function(){
+            if(this.checkLocalStorage()){
+                var temp = JSON.parse(localStorage.getItem('quizzer'));
+                this.getToPersModule(temp.stat, temp.actQuest, temp.actTest, temp.answArray, temp.passedTest);
+            }
+            else
+                this.getToPersModule({}, -1, -1, [], []);
+        };
 
 
-    window.PersModule = PersModule;
+        return PersModule;
 
-}(window));
+    });
