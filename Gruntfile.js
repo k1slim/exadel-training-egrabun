bf8ad333f9259c24bc8203284ab3ba27c6eb8d60
k1slim <k1slim@yandex.ru>
2014-08-08 13:16:12 +0400

    Testing

---
+++ Gruntfile.js
@@ -0,0 +1,101 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+
+        requirejs: {
+            compile: {
+                options: {
+                    mainConfigFile: "js/init.js",
+                    name: "init",
+                    include: ['init'],
+                    exclude:['jquery','lodash','handlebars','text'],
+                    out: "build/js/production.js"
+                }
+            }
+        },
+//        concat: {
+//            main: {
+//                src: [
+//                    'js/init.js',
+//                    'js/util.js',
+//                    'js/stat.js',
+//                    'js/router.js',
+//                    'js/persistence.js',
+//                    'js/test.js',
+//                    'js/constructor.js'
+//                ],
+//                dest: 'build/js/production.js'
+//            }
+//        },
+//        uglify: {
+//            main: {
+//                files: {
+//                    'build/js/production.min.js': '<%= concat.main.dest %>'
+//                }
+//            }
+//        },
+        csso: {
+            main: {
+                files: {
+                    'build/css/main.min.css': ['css/compiled/main.css']
+                }
+            }
+        },
+        jshint: {
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            },
+            all: [
+                'Gruntfile.js',
+                'js/*.js'
+            ]
+        },
+        handlebars: {
+            compile: {
+                files: {
+                    "build/template/resultTest.js": "template/templateListTest.hbs",
+                    "build/template/resultQuest.js": "template/templateQuest.hbs"
+                }
+            }
+        },
+        gitcommit: {
+            task: {
+                options: {
+                    message: 'Testing',
+                    noVerify: true,
+                    noStatus: false
+                },
+                files: {
+                    src: ['Gruntfile.js']
+                }
+            }
+        },
+        gitpush: {
+            task: {
+                options: {
+                    branch: 'gh-pages'
+                }
+            }
+        }
+
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-csso');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-handlebars');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.loadNpmTasks('grunt-git');
+
+    grunt.registerTask('default', ['requirejs',/*'concat', 'uglify',*/ 'csso', 'jshint','handlebars']);
+
+    grunt.registerTask('git', ['gitcommit','gitpush']);
+
+};
