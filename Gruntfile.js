34e6aef331b25207242e2448d2cc44ca205144da
k1slim <k1slim@yandex.ru>
2014-08-10 16:18:21 +0400

    Testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,13 +29,14 @@ module.exports = function(grunt){
 //                dest: 'build/js/production.js'
 //            }
 //        },
-//        uglify: {
-//            main: {
-//                files: {
-//                    'build/js/production.min.js': '<%= concat.main.dest %>'
-//                }
-//            }
-//        },
+        uglify: {
+            main: {
+                files: {
+                    'build/js/lib/require.min.js': 'build/js/lib/require.min.js',
+                    'build/js/lib/text.min.js': 'build/js/lib/text.min.js'
+                }
+            }
+        },
         csso: {
             main: {
                 files: {
@@ -78,12 +79,94 @@ module.exports = function(grunt){
             task: {
                 options: {
                     remote:'origin',
-                    upstream:true,
                     branch:'gh-pages'
                 }
             }
+        },
+        copy: {
+            main: {
+                files: [
+                    {
+                        src: 'index.html',
+                        dest: 'build/index.html'
+                    },
+                    {
+                        src: 'js/json/data.json',
+                        dest: 'build/js/json/data.json'
+                    },
+                    {
+                        src: 'bower_components/jquery/dist/jquery.min.js',
+                        dest: 'build/js/lib/jquery.min.js'
+                    },
+                    {
+                        src: 'bower_components/handlebars/handlebars.min.js',
+                        dest: 'build/js/lib/handlebars.min.js'
+                    },
+                    {
+                        src: 'bower_components/lodash/dist/lodash.min.js',
+                        dest: 'build/js/lib/lodash.min.js'
+                    },
+                    {
+                        src: 'bower_components/requirejs/require.js',
+                        dest: 'build/js/lib/require.min.js'
+                    },
+                    {
+                        src: 'bower_components/requirejs-text/text.js',
+                        dest: 'build/js/lib/text.min.js'
+                    },
+                    {
+                        src: 'template/templateListTest.hbs',
+                        dest: 'build/template/templateListTest.hbs'
+                    },
+                    {
+                        src: 'template/templateQuest.hbs',
+                        dest: 'build/template/templateQuest.hbs'
+                    }
+                ]
+            }
+        },
+        replace: {
+            index: {
+                src: ['build/index.html'],
+                overwrite: true,
+                replacements: [
+                    {
+                        from: /js\/init/i,
+                        to: "js/production.js"
+                    },
+                    {
+                        from: /css\/compiled\/main.css/i,
+                        to: "css/main.min.css"
+                    },
+                    {
+                        from: /bower_components\/requirejs\/require.js/i,
+                        to: "js/lib/require.min.js"
+                    }
+                ]
+            },
+            code: {
+                src: ['build/js/production.js'],
+                overwrite: true,
+                replacements: [
+                    {
+                        from: /..\/bower_components\/jquery\/dist\/jquery.min/i,
+                        to: "lib/jquery.min"
+                    },
+                    {
+                        from: /..\/bower_components\/handlebars\/handlebars/i,
+                        to: "lib/handlebars.min"
+                    }                    ,
+                    {
+                        from: /..\/bower_components\/lodash\/dist\/lodash/i,
+                        to: "lib/lodash.min"
+                    },
+                    {
+                        from: /..\/bower_components\/requirejs-text\/text/i,
+                        to: "lib/text.min"
+                    }
+                ]
+            }
         }
-//fdghgf
 
     });
 
@@ -93,11 +176,13 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-text-replace');
 
     grunt.loadNpmTasks('grunt-git');
 
-    grunt.registerTask('default', ['requirejs',/*'concat', 'uglify',*/ 'csso', 'jshint','handlebars']);
 
+    grunt.registerTask('build', ['jshint','copy','uglify','requirejs', 'csso','replace'/*,'handlebars'*/]);
     grunt.registerTask('git', ['gitcommit','gitpush']);
 
 };
